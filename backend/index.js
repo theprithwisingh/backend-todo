46aaa4d03397ea962fd6c7d477677b6ecf911391
theprithwisingh <prithwisingh77@gmail.com>
2025-04-18 23:11:42 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,18 @@
 import express from "express";
-import {pool} from 'pg'
+import {Pool} from 'pg'
+import { signinController, signupController, todoController } from "./controller/controller";
 const app  = express()
 const PORT  = 3000;
 
-
-  const connectionString = "postgresql://todo_owner:npg_l8gwZO9BiJsD@ep-bitter-river-a4wqx9qd-pooler.us-east-1.aws.neon.tech/todo?sslmode=require";
-  const pool = new Pool({
-    connectionString, // 👈 This is how you link with URL
+const pool = new Pool({
+    connectionString,
   });
-// app.use(cors());
-app.use(express.json());
-app.post("/sign", function(res,res) {
-    const username = req.body;
-    const password = req.body;
-    if()
 
-    res.json({ message: "Welcome to the Prithwi" });
-});
-app.listen(PORT, ()=>{
-    console.log(`server is runinng on PORT ${PORT}`)
-})
+app.use(cors());
+app.use(express.json());
+
+app.post("/login",signinController)
+
+app.post("/signup",signupController)
+
+app.get("/todo/:id",todoController)
